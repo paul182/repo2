53da1bfc3e0eac21984817fbaa17003a8ec033d2
praveenp <paul182@gmail.com>
2017-11-16 22:47:53 -0500

    Delete config.test.js

--- config.test.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    host: '127.0.0.1',
-    port: '3000',
-    dbUrl: 'mysql://b2vep6gxapuyluji:kwctx51trb9ximz9@lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a71145b8sirmpoc5'
-};
